f74a4ada9d6be06d9b5603004325955b10d496b5
Zebulun Arendsee <arendsee@users.noreply.github.com>
2024-06-19 07:39:44 -0400

    Revert top folder name to "docs"
    Apparently you have to use that particular name ...

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ script_dir=$(dirname "${0}");
 echo "Building site...";
 
 # Create the 'site' directory if it doesn't exist
-mkdir -p "${script_dir}/site/docs";
+mkdir -p "${script_dir}/docs/docs";
 
 # Copy files to the 'docs' directory
 for path in							\
@@ -17,13 +17,13 @@ for path in							\
 	"robots.txt"					\
 	"service-worker.js"				\
 ; do
-	cp -r "${script_dir}/src/${path}" "${script_dir}/site";
+	cp -r "${script_dir}/src/${path}" "${script_dir}/docs";
 done;
 
 # Build site
-asciidoctor -r asciidoctor-bibtex --doctype=book "${script_dir}/src/index.adoc" -a webfonts! -o "${script_dir}/site/docs/index.html"
+asciidoctor -r asciidoctor-bibtex --doctype=book "${script_dir}/src/index.adoc" -a webfonts! -o "${script_dir}/docs/docs/index.html"
 
 # Lazy load images
-sed -i -e 's/<img/<img loading="lazy"/g' "${script_dir}/site/docs/index.html";
+sed -i -e 's/<img/<img loading="lazy"/g' "${script_dir}/docs/docs/index.html";
 
 echo "Asciidoctor Jet site build complete!";
