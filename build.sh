9b55ceadd2c73d2655bdd999cc148e6d10bd3d6c
Zebulun Arendsee <arendsee@users.noreply.github.com>
2024-06-19 07:26:45 -0400

    Create a root "site" directory and "site/docs"

--- build.sh
+++ build.sh
@@ -6,26 +6,24 @@ script_dir=$(dirname "${0}");
 
 echo "Building site...";
 
-# Create the 'build' directory if it doesn't exist
-mkdir -p "${script_dir}/docs";
-
-# Remove all files from the 'docs' directory to prevent residual files
-rm -rf "${script_dir}/docs/"*;
+# Create the 'site' directory if it doesn't exist
+mkdir -p "${script_dir}/site/docs";
 
 # Copy files to the 'docs' directory
 for path in							\
+  "index.html" \
 	"static"						\
 	"manifest.webmanifest"			\
 	"robots.txt"					\
 	"service-worker.js"				\
 ; do
-	cp -r "${script_dir}/src/${path}" "${script_dir}/docs";
+	cp -r "${script_dir}/src/${path}" "${script_dir}/site";
 done;
 
 # Build site
-asciidoctor -r asciidoctor-bibtex --doctype=book "${script_dir}/src/index.adoc" -a webfonts! -o "${script_dir}/docs/index.html"
+asciidoctor -r asciidoctor-bibtex --doctype=book "${script_dir}/src/index.adoc" -a webfonts! -o "${script_dir}/site/docs/index.html"
 
 # Lazy load images
-sed -i -e 's/<img/<img loading="lazy"/g' "${script_dir}/docs/index.html";
+sed -i -e 's/<img/<img loading="lazy"/g' "${script_dir}/site/docs/index.html";
 
 echo "Asciidoctor Jet site build complete!";
